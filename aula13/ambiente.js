366c776915585d219a5616452fd8a26a487494a1
Matheus Campanhã <feliciano.ma01@gmail.com>
2022-08-18 18:11:05 -0300

    Aulas do curso JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+ var c = 1
+ do {
+     console.log(`Passo ${c}`)
+     c++
+ } while (c <= 6) // teste de comando no final
+
+
+/* teste de comando no início
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/ 
+
+
+
+/* estrutura de condição para repetir o mesmo comando
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
